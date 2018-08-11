f991a30a42ab5ec553a08298bc2054d766fa09a3
Moe Quraishi <moequraishi@gmail.com>
2018-08-10 23:20:13 -0700

    hunting github assignment fixes

--- assignments/hunting_github.js
+++ assignments/hunting_github.js
@@ -17,7 +17,7 @@
             // Notice that displayName is a function that takes in 1 parameter (this is the data that comes back from the API)
             function displayName(data) {
                 console.log(data);
-                $('.github--user').html('<h2>Your username is: '+ data.login +'</h2><h2>Location: '+ data.location +'</h2>');
+                $('.github--user').html('<h2 class="text-center">Username: '+ data.login +'</h2><h2 class="text-center">Location: '+ data.location +'</h2><h2 class="text-center">Blog: <a href="' + data.blog +'">'+ data.blog +'</a></h2><h2 class="text-center">Company: '+ data.company +'</h2>');
             }
         }
     });
