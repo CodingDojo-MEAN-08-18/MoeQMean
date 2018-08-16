9a82f5b2c681cb86d21f2efc939a0eaf62441e65
Moe Quraishi <moequraishi@gmail.com>
2018-08-15 18:29:00 -0700

    Cars and Cats assignment

---
+++ assignments/node/cars_and_cats/app.js
@@ -0,0 +1,81 @@
+// Setting necessary variables and imports.
+const http = require('http'),
+    fs = require('fs'),
+    port = 6789;
+
+// creating a server using http module:
+const server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+
+    // routing below
+    if (request.url === '/') {
+        fs.readFile('views/index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if (request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if (request.url === '/cats') {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('views/cars_new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/car_01.jpg'){
+        fs.readFile('./images/car_01.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/car_02.jpg'){
+        fs.readFile('./images/car_02.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cat_01.jpg'){
+        fs.readFile('./images/cat_01.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if(request.url === '/images/cat_02.jpg'){
+        fs.readFile('./images/cat_02.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }
+    else {
+        response.writeHead(404);
+        response.end('URL requested is not available.');
+    }
+});
+
+server.listen(port);
+console.log("Running in localhost at port: " + port);
