efeb738f6d2b53016bf44045db8297347ab923c5
Moe Quraishi <moequraishi@gmail.com>
2018-08-18 16:02:50 -0700

    Math Assignment fixes

--- assignments/node/math_module/app.js
+++ assignments/node/math_module/app.js
@@ -1,6 +1,7 @@
-var math = require('./mathlib');
+const MathLib = require('./mathlib');
+const math = new MathLib();
 
-math().add(2,3);
-math().multiply(3,5);
-math().square(5);
-math().random(1,35);
+math.add(2,3);
+math.multiply(3,5);
+math.square(5);
+math.random(1,35);
