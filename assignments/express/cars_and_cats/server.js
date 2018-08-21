eec6e69469e2b40a3afd8f3b505863eff3557c26
Moe Quraishi <moequraishi@gmail.com>
2018-08-20 20:47:27 -0700

    EJS Cat Data assignment

--- assignments/express/cars_and_cats/server.js
+++ assignments/express/cars_and_cats/server.js
@@ -10,14 +10,35 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/cats', function(req, res){
+app.get('/cats', function(req, res) {
     res.render('cats');
 });
 
-app.get('/cars', function(req, res){
+app.get('/ciggy', function(req, res) {
+    const cat_info = [
+        {favorite_food: "Marlboro Chow", age: 9, sleeping_spots: "In a dark alley next to a nightclub, asking people for cigarettes."}
+    ];
+   res.render('ciggy', {ciggy: cat_info})
+});
+
+app.get('/hipster', function(req, res) {
+    const cat_info = [
+        {favorite_food: "Molly Mix", age: 5, sleeping_spots: "Inside of a nightclub, right next to the DJ booth."}
+    ];
+    res.render('hipster', {hipster: cat_info})
+});
+
+app.get('/catso', function(req, res) {
+    const cat_info = [
+        {favorite_food: "Doggy Treats", age: 35, sleeping_spots: "The whole town is his, u gotta problem?"}
+    ];
+    res.render('catso', {catso: cat_info})
+});
+
+app.get('/cars', function(req, res) {
     res.render('cars');
 });
-app.get('/form', function(req, res){
+app.get('/form', function(req, res) {
     res.render('form');
 });
 
