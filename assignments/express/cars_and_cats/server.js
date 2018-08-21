e54cadb7036bfd21fe9eb667de6c25faaae4976f
Moe Quraishi <moequraishi@gmail.com>
2018-08-20 20:20:26 -0700

    cars and cats assignment with express and ejs

--- assignments/express/cars_and_cats/server.js
+++ assignments/express/cars_and_cats/server.js
@@ -3,21 +3,24 @@ const express = require("express"),
 
 app.use(express.static(__dirname + "/static"));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.render('static/index.html');
+    res.render('index');
 });
 
 app.get('/cats', function(req, res){
-    res.render('static/cats.html');
+    res.render('cats');
 });
 
 app.get('/cars', function(req, res){
-    res.render('static/cars.html');
+    res.render('cars');
+});
+app.get('/form', function(req, res){
+    res.render('form');
 });
 
-app.get('/form', function(req, res){
-    res.render('static/form.html');
-});
 
 app.listen(8000, function(){
     console.log("Listening on port: 8000");
