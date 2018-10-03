064f5498e7a246025a07c8ffcb89ec487fea03bc
Moe Quraishi <moequraishi@gmail.com>
2018-10-02 21:53:18 -0700

    fixes

--- assignments/mean-exam/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-  app = express(),
-  port = 1337,
-  routes = require('./server/routes/routes')(app);
-
-app.listen(port, function() {
-  console.log('Listening on port: '+ port);
-});
